65f37e3b5aa21d55ae3e03d13350b5655d5b7d68
Yu Yanagida <cappuccino1204.ck@gmail.com>
2023-05-16 23:28:29 +0900

    create new bsdf file

---
+++ Source/Falcor/Rendering/Materials/PLT/PLTDiffuseFluorescenceMaterial.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+
+ **************************************************************************/
+#pragma once
+#include "Scene/Material/BasicMaterial.h"
+
+namespace Falcor
+{
+    /** This class implements a Lambertian diffuse material, where
+        reflectance does not depend on wo.
+
+        Texture channel layout:
+
+            BaseColor
+                - RGB - Base Color
+            Normal
+                - 3-Channel standard normal map, or 2-Channel BC5 format
+    */
+    class FALCOR_API PLTDiffuseMaterial : public BasicMaterial
+    {
+    public:
+        using SharedPtr = std::shared_ptr<PLTDiffuseMaterial>;
+
+        /** Create a new PLTDiffuse material.
+            \param[in] name The material name.
+        */
+        static SharedPtr create(std::shared_ptr<Device> pDevice, const std::string& name = "");
+
+        Program::ShaderModuleList getShaderModules() const override;
+        Program::TypeConformanceList getTypeConformances() const override;
+
+        virtual bool renderUI(Gui::Widgets& widget, const Scene *scene) override;
+
+    protected:
+        PLTDiffuseMaterial(std::shared_ptr<Device> pDevice, const std::string& name);
+    };
+}
